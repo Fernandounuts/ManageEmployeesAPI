465c8b39a6ed45da86be115c9d5495dc8abf59fc
Fernandounuts <fernado.balieiro0580@hotmail.com>
2023-10-29 18:15:21 -0300

    interface do IFuncionario sendo feita

--- Interfaces/IFuncionarioRepository.cs
+++ Interfaces/IFuncionarioRepository.cs
@@ -6,13 +6,13 @@ namespace WorkOp.Interfaces;
 public interface IFuncionarioRepository {
     public Task<List<Funcionario>> GetAllFuncionarios(long? empresaId);
 
-    public Task<IActionResult> GetFuncionarioById(long? empresaId, long? funcionarioId);
+    public Task<List<Funcionario>> GetFuncionarioById(long? empresaId, long? funcionarioId);
 
-    public Task<IActionResult> GetFuncionariosInDepartametn(long? departamentoId);
-    public Task<List<IActionResult>> GetFuncionariosByIdInDepartamento(long? departamentoId, long? funcionarioId);
+    public Task<List<Funcionario>> GetFuncionariosInDepartameto(long? departamentoId);
+    public Task<Funcionario> GetFuncionariosByIdInDepartamento(long? departamentoId, long? funcionarioId);
 
-    public Task<IActionResult> GetFuncionariosByIdInProjeto(long? funcionarioId, long? projetoId);
-    public Task<List<IActionResult>> GetFuncionariosByProjeto(long? departamentoId);
+    public Task<Funcionario> GetFuncionariosByIdInProjeto(long? funcionarioId, long? projetoId);
+    public Task<List<Funcionario>> GetFuncionariosByProjeto(long? departamentoId);
 
     public Task<bool> FuncionarioExists(long? funcionarioId);
 }
